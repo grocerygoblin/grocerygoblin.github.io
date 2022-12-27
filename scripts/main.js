843ca0226bce6a557cc5e979d03f5ca8463f7d6c
grocerygoblin <grocerysynchronization@gmail.com>
2022-12-26 19:10:30 -0600

    testing js event

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const myImage = document.querySelector("img");
 myImage.onClick = () => {
     const mySrc = myImage.getAttribute("src");
     if (mySrc === "\images\cross.png") {
-        myImage.setAttribute("src", "\images\heart.png");
+        myImage.setAttribute("src", "https://m.media-amazon.com/images/M/MV5BY2Y0YTlkZmMtZDMwZi00MDQwLThmYmUtZjQzMjRlMmQzMTNiXkEyXkFqcGdeQXVyNTgxODY5ODI@._V1_.jpg");
     } else {
         myImage.setAttribute("src", "\images\cross.png");
     }
