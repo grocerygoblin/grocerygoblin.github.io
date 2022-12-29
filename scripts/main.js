4acb94d5e159e1dec97b4ac65ac47b1f754979d5
MINICARNIGEL <grocerysynchronization@gmail.com>
2022-12-28 18:15:41 -0600

    add a username fetcher and a toggleable image

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,36 @@
 const myImage = document.querySelector("img");
 
-myImage.onClick = () => {
+myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src");
-    if (mySrc === "\images\cross.png") {
-        myImage.setAttribute("src", "https://m.media-amazon.com/images/M/MV5BY2Y0YTlkZmMtZDMwZi00MDQwLThmYmUtZjQzMjRlMmQzMTNiXkEyXkFqcGdeQXVyNTgxODY5ODI@._V1_.jpg");
+    if (mySrc === "images/cross.png") {
+        myImage.setAttribute("src", "images/heart.png");
     } else {
-        myImage.setAttribute("src", "\images\cross.png");
+        myImage.setAttribute("src", "images/cross.png");
     }
-};  
+}; //toggles images of cross and sacred heart back and forth on click
+
+let myHeading = document.querySelector("h1");
+let myButton = document.querySelector("button"); 
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem ("name", myName);
+        myHeading.textContent = `God loves you, $(myName)`;
+    }
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `God loves you, ${myName}`;
+} //set username prompt
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `God loves you, ${storedName}`;
+} //checks if username is stored, if so, displays stored name. if not, runs username prompt
+
+myButton.onclick = () => {
+    setUserName();
+}; //runs setUserName on button click
